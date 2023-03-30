b265493b4ae551b08be4a257e3cb440f2a8586e6
yulia-kosiuta <124287540+yulia-kosiuta@users.noreply.github.com>
2023-03-30 16:46:33 +0000

    added write-us.css

--- js/testimonials-slider.js
+++ js/testimonials-slider.js
@@ -3,14 +3,14 @@ const sliderBody = document.querySelector('.slider__body');
 document.querySelector('.next-button').addEventListener('click', function () {
     changePosition -= 100;
     if (changePosition < -200) {
-        changePosition = 0;
+        changePosition = -200;
     }
     sliderBody.style.left = changePosition + '%';
 })
 document.querySelector('.prev-button').addEventListener('click', function () {
     changePosition += 100;
     if (changePosition > 0) {
-        changePosition = -200;
+        changePosition = 0;
     }
     sliderBody.style.left = changePosition + '%';
 })
